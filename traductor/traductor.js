e18c24ec230408a0cbb1fd991c8db79a7a8478c5
Driss <driss.landou@gmail.com>
2025-07-04 15:22:38 +0200

    Update traductor.js

--- traductor/traductor.js
+++ traductor/traductor.js
@@ -7,7 +7,7 @@ async function translating() {
         
         const langPair = (sourceLang && sourceLang !== "auto") ? `${sourceLang}|${targetLang}` : `|${targetLang}`;
         const url = `https://api.mymemory.translated.net/get?q=
-            ${encodeURIComponent(input)}&langpair=${langPair}`;
+            ${encodeURIComponent(input)}&langpair=${langPair}`; // actual request (no up P for the after &)
         
         // Log the request§
         
@@ -51,57 +51,7 @@ async function translating() {
 
 
 
-
-
-
-
-
-
-// async function translating() {
-//     try {
-//         const input = await document.getElementById("sourceText").value
-//         const sourceLang = await document.getElementById("sourceLang").value
-//         const targetLang = await document.getElementById("targetLang").value
-//         const Key = "dd094ff4-beb0-4fe9-a298-e5f3ffc36c96:fx";
-//         // to secure the key, I migth be able to put it in some api, or put it in my own api on a ras pi
-//         const url = "https://api-free.deepl.com/v2/translate";
-        
-        
-        
-//         const response = await fetch(url, {
-//             method: "POST",
-//             body: data
-//         });
-        
-//         const data = new URLSearchParams();
-//         data.append("auth_key", `${Key}`);
-//         data.append("text", `${input}`);
-//         data.append("target_lang", `${targetLang}`);  //maybe this is the synthax of this value??
-//         data.append("source_lang", `${sourceLang}`);
-//         console.log(data.toString());
-        
-        
-// // document.getElementById("code_response").innerHTML = response.translations;
-// const jsonResponse = await response.json();
-// document.getElementById("translatedText").innerHTML = jsonResponse.translations[0].text;
-
-// // document.getElementById("code_response").innerHTML = JSON.stringify(jsonResponse);
-
-// // document.getElementById("code_input").innerHTML = JSON.stringify(input);
-// // document.getElementById("code_target_lang").innerHTML = JSON.stringify(targetLang);
-// // if (sourceLang) {
-// //     document.getElementById("code_source_lang").innerHTML = JSON.stringify(sourceLang);
-// // } else {document.getElementById("code_source_lang").innerHTML ='"AUTO"'}
-    
-// // } catch (error) {console.error("this is the error: ", error);
-// //     throw error;}
-    
-// // };
-
-
-
-
-
+//this is for the previous api, if remake, put key
 // // const response = await fetch(url, {
 //     //     method: "POST",
 //     //     body: new URLSearchParams({
